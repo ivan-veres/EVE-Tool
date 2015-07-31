c4f976340d6119f2be4cd780995c982d3b58d369
iVeres <ivan.veres.dj@gmail.com>
2015-07-31 22:16:47 +0200

    Login

--- Routers/Home.php
+++ Routers/Home.php
@@ -18,7 +18,18 @@ class Home extends Route
      */
     public function index()
     {
-        $this->view->user = $this->db->query('SELECT username, last_login FROM users', []);
+
+        $this->db->query('INSERT INTO users (email, username, password, role, last_login) VALUES (:email, :name, :pass, :role, :last)',
+            array(
+                'email' => 'sima@ind2.dev',
+                'name'  => 'sima',
+                'pass'  => md5('annaosk'.'dragonball'),
+                'role'  => 'admin',
+                'last'  => date("Y-m-d H:i:s"),
+            ));
+
+        $this->view->user = $this->db->query('SELECT * FROM users', '');
+        $this->view->username = $this->view->user['username'];
 
         $this->view->render('home/home');
     }
